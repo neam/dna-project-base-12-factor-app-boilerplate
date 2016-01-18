7760940a85f632b8fc11a541c283db977d246582
Fredrik Wollsén <fredrik@neam.se>
2016-01-18 15:59:07 +0200

    Dna project base sync + composer updates

--- stack/start-local-dev.sh
+++ stack/start-local-dev.sh
@@ -3,7 +3,10 @@
 # fail on any error
 set -o errexit
 
-docker-machine start default
+status=$(docker-machine status default)
+if [ "$status" != "Running" ]; then
+  docker-machine start default
+fi
 eval "$(docker-machine env default)"
 stack/start.sh
 bin/angular-frontend-develop.sh $@
